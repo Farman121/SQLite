ef7cdcb5dfafb0760cab49808b623dbf1e611536
Farman121 <afaankhan055@gmail.com>
2020-04-17 22:17:42 +0500

    Initial commit

---
+++ app/src/main/java/com/afaan/sql/ModelClass.java
@@ -0,0 +1,30 @@
+package com.afaan.sql;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap;
+
+public class ModelClass {
+    private String ImageName;
+    private Bitmap image;
+
+    public ModelClass(String imageName, Bitmap image) {
+        ImageName = imageName;
+        this.image = image;
+    }
+
+    public String getImageName() {
+        return ImageName;
+    }
+
+    public void setImageName(String imageName) {
+        ImageName = imageName;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+}
